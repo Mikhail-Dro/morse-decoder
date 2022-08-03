878aa3196d8067ade9c9b64eee6b7602ddff3c3c
Mikhail Dro <droydbangs@gmail.com>
2022-08-03 22:50:16 +0300

    feat: compleat task_ morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let currentPosition = 0;
+    let ans = '';
+    let letterCode = '';
+    let letterMorse = '';
+    let charMorse = '';
+    
+    for (let i = 0; i < ( expr.length ) / 10; i++) {
+    
+        if (expr[currentPosition] == '*') {
+            ans += ' ';
+            j = 10;
+            currentPosition += 10;
+        } else {
+            for (let j = 0; j < 5; j++) {
+        
+                for (let k = 0; k < 2; k++) {
+                    letterCode += expr[currentPosition];
+                    currentPosition += 1;
+                }
+                if (letterCode == '10') {
+                    charMorse = '.';
+                } else if (letterCode == '11') {
+                    charMorse = '-';
+                }
+                letterMorse += charMorse;
+                letterCode = '';
+                charMorse = '';
+            }
+            ans += MORSE_TABLE[letterMorse];
+            letterMorse = '';
+        }
+    }
+    return(ans);
 }
 
 module.exports = {
